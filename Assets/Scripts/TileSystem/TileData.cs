ef5cf82e3fd5c065093f5e8a2a74ed4de44d6e2a
Alex Wang <wangtianyoualex@gmail.com>
2024-03-11 01:41:34 -0700

    Add_TileSpawnLogic_03/11/2024

--- Assets/Scripts/TileSystem/TileData.cs
+++ Assets/Scripts/TileSystem/TileData.cs
@@ -6,21 +6,23 @@ public class TileData
     public int TileGridDimension { get; private set; }
     public Vector2 TileGridCenterXZ { get; private set; }
     public Tile[] TileGrid { get; private set; }
+
+    private Vector2[] _tileVerts;
     public TileData(Vector2 _center, int _dimension, float _tileSize)
     {
         TileSize = _tileSize;  
         TileGridDimension = _dimension;
         TileGridCenterXZ = _center;
     }
-    public void ConstructTileGrid() 
+    public void ConstructTileGrid()
     {
         Tile[] tiles = new Tile[TileGridDimension * TileGridDimension];
         float offset = -TileGridDimension * TileSize / 2 + TileSize / 2;
         Vector2 botLeftCorner = TileGridCenterXZ + new Vector2(offset, offset);
-        
-        for (int x = 0; x < TileGridDimension; x++) 
+
+        for (int x = 0; x < TileGridDimension; x++)
         {
-            for (int y = 0; y < TileGridDimension; y++) 
+            for (int y = 0; y < TileGridDimension; y++)
             {
                 Vector2 tilePos = botLeftCorner + new Vector2(TileSize * x, TileSize * y);
                 tiles[x * TileGridDimension + y] = new Tile(TileSize, tilePos);
@@ -28,6 +30,22 @@ public class TileData
         }
         TileGrid = tiles;
     }
+    public Vector2[] GetTileVerts() 
+    {
+        if (TileGrid == null)
+            return null;
+        _tileVerts = new Vector2[TileGridDimension * TileGridDimension * 4];
+        int i = 0;
+        foreach (Tile t in TileGrid) 
+        {
+            Vector2[] corners = t.GetTileCorners();
+            for (int j = 0;j < 4;j++)
+                _tileVerts[i + j] = corners[j];
+             i+=4;
+        }
+        return _tileVerts;
+    }
+   
 }
 
 public class Tile 
@@ -44,6 +62,15 @@ public class Tile
     public Vector3 GetTilePosSize() 
     {
         return new Vector3(_tilePosition.x, _tilePosition.y, _tileSize);
+    }
+    public Vector2[] GetTileCorners() 
+    {
+        Vector2[] corners = new Vector2[4];
+        corners[0] = _tilePosition - Vector2.one * _tileSize / 2;
+        corners[1] = _tilePosition + new Vector2 (1,-1) * _tileSize / 2;
+        corners[2] = _tilePosition + Vector2.one * _tileSize / 2;
+        corners[3] = _tilePosition + new Vector2(-1,1) * _tileSize / 2;
+        return corners;
     }
 
 }
--- Assets/Scripts/TileSystem/TileManager.cs
+++ Assets/Scripts/TileSystem/TileManager.cs
@@ -6,13 +6,26 @@ public class TileManager : MonoBehaviour
     private TileData _tileData;
     private TileVisualizer _tileVisualizer;
     public float TileSize;
-    public int TileGridDimention;
+    public int TileGridDimension;
     public Vector2 TileGridCenterXZ;
-    public Material RenderMaterial;
+    public Material DebugMaterial;
+
+    public Mesh SpawnMesh;
+    public Material SpawnMeshMaterial;
+    public int SpawnSubivisions = 3;
+
+    private ComputeShader _spawnOnTileShader;
+    private ComputeBuffer _vertBuffer;
+    private ComputeBuffer _spawnBuffer;
+    private ComputeBuffer _argsBuffer;
+    private uint[] _args;
+    private Vector3[] _spawnPos;
 
     private void OnEnable()
     {
+        _spawnOnTileShader = (ComputeShader)Resources.Load("CS_SpawnOnTile");
         SetupTileGrid();
+        SetupTileCompute();
         SetupTileVisual();
     }
     private void OnDisable()
@@ -22,19 +35,67 @@ public class TileManager : MonoBehaviour
     private void Update()
     {
         DrawTileVisual();
+        DrawIndirect();
     }
-
+ 
     void SetupTileGrid() 
     {
-        _tileData = new TileData(TileGridCenterXZ, TileGridDimention, TileSize);
+        _tileData = new TileData(TileGridCenterXZ, TileGridDimension, TileSize);
         _tileData.ConstructTileGrid();
         
+    }
+    void SetupTileCompute()
+    {
+        if (
+           SpawnMesh == null
+           || SpawnMeshMaterial == null
+           )
+            return;
+        int tileCount = TileGridDimension * TileGridDimension;
+        int instancePerTile = SpawnSubivisions * SpawnSubivisions;
+        _vertBuffer = new ComputeBuffer(tileCount * 4, sizeof(float) * 2);
+        _vertBuffer.SetData(_tileData.GetTileVerts());
+
+        _spawnPos = new Vector3[tileCount * instancePerTile];
+        _spawnBuffer = new ComputeBuffer(tileCount * instancePerTile, sizeof(float) * 3);
+        _spawnBuffer.SetData(_spawnPos);
+
+        _argsBuffer = new ComputeBuffer(1, sizeof(uint) * 5, ComputeBufferType.IndirectArguments);
+        _args = new uint[] {
+            SpawnMesh.GetIndexCount(0),
+            (uint)(tileCount * instancePerTile),
+            SpawnMesh.GetIndexStart(0),
+            SpawnMesh.GetBaseVertex(0),
+            0
+        };
+        _argsBuffer.SetData(_args);
+
+        _spawnOnTileShader.SetInt("_NumTiles", tileCount);
+        _spawnOnTileShader.SetInt("_Subdivisions", SpawnSubivisions);
+        _spawnOnTileShader.SetBuffer(0, "_VertBuffer", _vertBuffer);
+        _spawnOnTileShader.SetBuffer(0, "_SpawnBuffer", _spawnBuffer);
+        _spawnOnTileShader.Dispatch(0, Mathf.CeilToInt(tileCount / 128f), 1, 1);
+        SpawnMeshMaterial.SetBuffer("_SpawnBuffer", _spawnBuffer);
+
+    }
+    void DrawIndirect() 
+    {
+
+        if (
+            SpawnMesh == null
+            || SpawnMeshMaterial == null
+            || _argsBuffer == null
+            )
+            return;
+        Bounds bounds = new Bounds(Vector3.zero, Vector3.one * 100f);
+        Graphics.DrawMeshInstancedIndirect(SpawnMesh, 0,SpawnMeshMaterial, bounds, _argsBuffer,
+            0, null, UnityEngine.Rendering.ShadowCastingMode.On, true, 0, null, UnityEngine.Rendering.LightProbeUsage.BlendProbes);
     }
     void SetupTileVisual() 
     {
         if (_tileData == null)
             return;
-        _tileVisualizer = new TileVisualizer(_tileData.TileGrid,RenderMaterial, _tileData.TileGridDimension);
+        _tileVisualizer = new TileVisualizer(_tileData.TileGrid,DebugMaterial, _tileData.TileGridDimension);
         _tileVisualizer.VisualizeTiles();
     }
     void DrawTileVisual()
@@ -48,5 +109,8 @@ public class TileManager : MonoBehaviour
         if (_tileVisualizer == null)
             return;
         _tileVisualizer.ReleaseBuffer();
+        _vertBuffer?.Dispose();
+        _argsBuffer?.Dispose();
+        _spawnBuffer?.Dispose();
     }
 }
